2ea8341c3d0b5767363c5846fc60b44fe4615649
binora <binny@mindseed.in>
2018-05-26 14:42:06 +0800

    removing uneccessary update_fn

--- lib/commands/lists/impl.ex
+++ lib/commands/lists/impl.ex
@@ -1,8 +1,6 @@
 defmodule Rediex.Commands.Lists.Impl do
   alias Rediex.Commands.Helpers
 
-  def update_fn(list, values), do: values ++ list
-
   def execute(:lpush, [key | values], state) do
     list = reverse(values) ++ (state[key] || [])
     {:ok, length(list), Helpers.set(state, key, list)}
